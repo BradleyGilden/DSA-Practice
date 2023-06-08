96e7dd898a8f6ffe806d3662c5f391d458df2a94
BradleyGilden <bradleygilden@gmail.com>
2023-06-08 21:55:31 +0200

    linked list methods

---
+++ linked_lists/Python/llist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        if self.head is None:
+            return
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+
+    def add_node_beginning(self, data):
+        new = Node(data)
+        new.ref = self.head
+        self.head = new
+
