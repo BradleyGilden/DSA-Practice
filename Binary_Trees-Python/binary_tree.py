8b469d4919e07f1aa7bdeb19e0f01493fc8aed55
BradleyGilden <bradleygilden@gmail.com>
2024-02-15 17:40:43 +0200

    setting up binary tree structure

--- Binary_Trees-Python/binary_tree.py
+++ Binary_Trees-Python/binary_tree.py
@@ -7,21 +7,26 @@ Author: Bradley Dillion Gilden
 Date: 22-10-2023
 """
 
+
 class Node:
     """represents nodes in a binary tree"""
-    def __init__(self, data):
-        self.parent = None
-        self.left = None
-        self.right = None
+    def __init__(self, data, left=None, right=None):
+        self.left = left
+        self.right = right
         self.data = data
+
 
 class BinaryTree:
     """contains all the methods necessary for working on binary trees"""
-    def __init__(self, rootVal):
-        self.root = Node(rootVal)
+    def __init__(self, data=None, left=None, right=None):
+        self.root = None
+        if (data):
+            self.root = Node(data)
 
-    def add_left(self, data):
-        self.left = Node(data)
-
-
-if __name__ == '__main__':
+    def print(self):
+        def printTree(node, level):
+            if node is not None:
+                printTree(node.right, level + 1)
+                print(' ' * 4 * level + '-> ' + str(node.data))
+                printTree(node.left, level + 1)
+        printTree(self.root, 0)
---
+++ Binary_Trees-Python/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+This file is for testing the Binary Tree structure
+
+Author: Bradley Dillion Gilden
+Date: 15-02-2024
+"""
+from binary_tree import BinaryTree, Node
+
+
+if __name__ == '__main__':
+    bt = BinaryTree(1)
+    bt.root.left = Node(32, Node(24, Node(9), Node(3)), Node(2))
+    bt.root.right = Node(11, Node(39), Node(74))
+
+    bt.print()
