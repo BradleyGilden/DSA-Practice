de37caab2eb82544953cba612fe3706562cf2570
BradleyGilden <bradleygilden@gmail.com>
2024-06-27 23:33:54 +0200

    adj list

---
+++ Graphs/DFS/dfs.md
@@ -0,0 +1,2 @@
+1.
+  <img src="https://i.ytimg.com/vi/7fujbpJ0LB4/maxresdefault.jpg">
---
+++ Graphs/DFS/dfs.py
@@ -0,0 +1,23 @@
+"""
+DFS - Depth First Search Algorithm
+
+see dfs.md for diagrams
+"""
+
+
+def depth_first_search(graph):
+    """prints nodes using dfs"""
+    n = len(graph)
+    adj_list = [
+        [1, 9],
+        [0, 8]
+        [3],
+        [2, 4],
+        [3],
+        [3, 6],
+        [7, 5],
+        [3, 6],
+        [1, 7, 9]
+        [7, 11]
+        [10, 7]
+    ]
