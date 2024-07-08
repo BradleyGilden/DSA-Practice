7b3d59544c1db1ff9555e7aa608de08003dd1fb6
BradleyGilden <bradleygilden@gmail.com>
2024-07-08 15:58:31 +0200

    haspath with bfs traverse

--- Graphs/DFS_BFS/challenges.py
+++ Graphs/DFS_BFS/challenges.py
@@ -5,12 +5,29 @@ from custom_structs import Stack, Queue  # noqa
 
 
 def hasPath(graph: dict, start: str, end: str):
-    # init stack
+    """ Depth First Version
+    """
     if (start == end):
         return True
     for neighbour in graph[start]:
         if hasPath(graph, neighbour, end):
             return True
+
+    return False
+
+
+def hasPath_v2(graph: dict, start: str, end: str):
+    """ Breadth First Version
+    """
+    # init queue
+    queue = Queue(start)
+
+    while (len(queue)):
+        current = queue.dequeue()
+        if (current == end):
+            return True
+        for neighbour in graph[current]:
+            queue.enqueue(neighbour)
 
     return False
 
@@ -26,3 +43,5 @@ if __name__ == '__main__':
     }
     print(hasPath(graph, 'k', 'f'))
     print(hasPath(graph, 'i', 'g'))
+    print(hasPath_v2(graph, 'k', 'f'))
+    print(hasPath_v2(graph, 'f', 'k'))
