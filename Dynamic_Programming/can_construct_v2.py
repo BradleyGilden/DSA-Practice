483cfba076c05b2c1448f9079d445ecbd003fad5
bradleygilden <bradleygilden@gmail.com>
2024-07-21 10:33:39 +0000

    can construct 2nd variation

--- Dynamic_Programming/can_construct_v2.py
+++ Dynamic_Programming/can_construct_v2.py
@@ -2,8 +2,8 @@
 Write a function `canConstruct(target, wordBank)` that accepts a target string and an
 array of strings.
 
-The function should return a boolean indicating whether or not the `target` can
-be constructed by concatenating elements of the `wordBank` array.
+The function should return an int indicating how many combinations of words from the
+wordbank can be constructed to form the target, if any.
 
 You may reuse elements of wordBank as many times as needed.
 """
@@ -18,27 +18,26 @@ def canConstruct(target, wordbank, memo=None):
         return memo[target]
 
     if not target:
-        return True
+        return 1
 
+    count = 0
     for word in wordbank:
         if target.startswith(word):
-            if canConstruct(target[len(word) :], wordbank, memo):
-                memo[target] = True
-                return True
+            count += canConstruct(target[len(word) :], wordbank, memo)
 
-    memo[target] = False
-    return False
+    memo[target] = count
+    return count
 
 
 if __name__ == "__main__":
-    print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))  # true
-    print(canConstruct("abcabdef", ["ab", "abc", "cd", "def", "abcd"]))  # true
+    print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef"]))  # 3
+    print(canConstruct("abcabdef", ["ab", "abc", "cd", "def", "abcd"]))  # 1
     print(
         canConstruct("abcabbdef", ["ab", "abc", "cd", "def", "abcd"])
-    )  # false
+    )  # 0
     print(
         canConstruct("skateboard", ["bo", "ate", "rd", "t", "sk", "boar"])
-    )  # false
+    )  # 0
     print(
         canConstruct(
             "eeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
@@ -51,7 +50,7 @@ if __name__ == "__main__":
                 "eeeeee",
             ],
         )
-    )  # false
+    )  # 0
 
 """
 m = target length
@@ -64,6 +63,6 @@ O(m^2) space
 
 Brute Force Solution
 --------------------
-O(n * m) time
+O(n * m^2) time
 O(m^2) space
 """
